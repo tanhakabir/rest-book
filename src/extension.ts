cd223d6df16d390123f15010e102fbb85644e6f5
Tanha Kabir <tanhakabir.ca@gmail.com>
2020-12-09 13:17:07 -0800

    Remove unused files

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { commandRESTCall } from './commandRESTCall';
 import { CallsNotebookProvider } from './notebookProvider';
 import { logDebug } from './common';
 
