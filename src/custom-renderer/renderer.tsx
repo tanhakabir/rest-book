9e8c2e38d34b3dae6a4d2a51638f5f481c7b233f
Tanha Kabir <tanhakabir.ca@gmail.com>
2021-01-11 23:25:41 -0800

    add dynamic headers and config tab

--- src/custom-renderer/renderer.tsx
+++ src/custom-renderer/renderer.tsx
@@ -4,32 +4,64 @@ import { useMemo, useState } from 'preact/hooks';
 import { ResponseRendererElements } from '../common/response';
 
 export const Response: FunctionComponent<{ response: Readonly<ResponseRendererElements> }> = ({ response }) => {
-    const [activeIndex, setActive] = useState(1);
+    const [activeIndex, setActive] = useState(0);
+
+    const renderTabHeaders = () => {
+        let result: h.JSX.Element[] = [];
+
+        result.push(<button class='tab-header' onClick={() => setActive(0)}>Data</button>);
+
+        if(response.headers) {
+            result.push(<button class='tab-header' onClick={() => setActive(1)}>Headers</button>);
+        }
+
+        if(response.config) {
+            result.push(<button class='tab-header' onClick={() => setActive(2)}>Config</button>);
+        }
+
+        return result;
+    };
 
     return <div>
         <Status code={response.status} text={response.statusText} request={response.request} />
         <div id='tab-bar'>
-          <button class='tab-header' onClick={() => setActive(0)}>
-            Headers
-          </button>
-          <button class='tab-header' onClick={() => setActive(1)}>
-            Data
-          </button>
+          {renderTabHeaders()}
         </div>
-        <HeadersTab headers={response.headers} active={activeIndex === 0}/>
-        <DataTab data={response.data} active={activeIndex === 1}/>
+        <DataTab data={response.data} active={activeIndex === 0}/>
+        <HeadersTab headers={response.headers} active={activeIndex === 1}/>
+        <ConfigTab config={response.config} active={activeIndex === 2}/>
     </div>;
 };
 
 const Status: FunctionComponent<{ code: number, text: string, request?: any}> = ({ code, text, request }) => {
     return <div>
-        {code} {text} / {request.res.responseUrl}
+        {code} {text} / {request.res.responseUrl} {request.res.httpVersion}
     </div>;
 };
 
 const HeadersTab: FunctionComponent<{ headers?: any, active: boolean}> = ({ headers, active }) => {
     const renderFields = () => { return Object.keys(headers).map((key) => {
         return <li>{key} :  {headers[key]}</li>;
+    })};
+
+    //@ts-ignore
+    return <div class='tab-content' hidden={!active}>
+        <ul>
+            {renderFields()}
+        </ul>
+    </div>;
+};
+
+const ConfigTab: FunctionComponent<{ config?: any, active: boolean}> = ({ config, active }) => {
+    const renderFields = () => { return Object.keys(config).map((key) => {
+        if(key === 'headers') {
+            return <ul>
+                {Object.keys(config.headers).map((hKey) => {
+                    return <li>{hKey} :  {config.headers[hKey]}</li>;
+                })}
+            </ul>;
+        }
+        return <li>{key} :  {config[key]}</li>;
     })};
 
     //@ts-ignore
