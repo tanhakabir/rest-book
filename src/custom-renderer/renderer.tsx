caa6520ae690a74c556da332dad83c82e5af8dcf
tanhakabir <tanhakabir.ca@gmail.com>
2021-01-11 23:44:37 -0800

    add simple custom renderer with no styling (#44)
    * remove unnecessary comment
    
    * add autocompletions for MIME types
    
    * checkpoint
    
    * update out of data dependency
    
    * checkpoint, need webpack next
    
    * checkpoint
    
    * refractor organization
    
    * checkpoint
    
    * checkpoint
    
    * renders custom output
    
    * fix debug config
    
    * display all data
    
    * add basic tabs
    
    * add dynamic headers and config tab
    
    * mini refractor table pane
    
    * highlight tab on

--- src/custom-renderer/renderer.tsx
+++ src/custom-renderer/renderer.tsx
@@ -1,15 +1,72 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { FunctionComponent, h } from 'preact';
 import { useMemo, useState } from 'preact/hooks';
 import { ResponseRendererElements } from '../common/response';
 
 export const Response: FunctionComponent<{ response: Readonly<ResponseRendererElements> }> = ({ response }) => {
+    const [activeIndex, setActive] = useState(0);
+
+    const renderTabHeaders = () => {
+        let result: h.JSX.Element[] = [];
+
+        //@ts-ignore
+        result.push(<button class='tab-header' onClick={() => setActive(0)} active={activeIndex === 0}>Data</button>);
+
+        if(response.headers) {
+            //@ts-ignore
+            result.push(<button class='tab-header' onClick={() => setActive(1)}  active={activeIndex === 1}>Headers</button>);
+        }
+
+        if(response.config) {
+            //@ts-ignore
+            result.push(<button class='tab-header' onClick={() => setActive(2)}  active={activeIndex === 2}>Config</button>);
+        }
+
+        return result;
+    };
+
     return <div>
-        <Status code={response.status} text={response.statusText} request={response.request}></Status>
+        <Status code={response.status} text={response.statusText} request={response.request} />
+        <br />
+        <div id='tab-bar'>
+          {renderTabHeaders()}
+        </div>
+        <br />
+        <DataTab data={response.data} active={activeIndex === 0}/>
+        <TableTab dict={response.headers} active={activeIndex === 1}/>
+        <TableTab dict={response.config} active={activeIndex === 2}/>
     </div>;
 };
 
 const Status: FunctionComponent<{ code: number, text: string, request?: any}> = ({ code, text, request }) => {
     return <div>
-        {code} {text} / {request.res.responseUrl}
+        {request.method} {code} {text}   /   {request.res.responseUrl}
+    </div>;
+};
+
+const TableTab: FunctionComponent<{ dict?: any, active: boolean}> = ({ dict, active }) => {
+    const renderFields = () => { return Object.keys(dict).map((key) => {
+        if(typeof dict[key] === 'object') {
+            return <ul>
+                {Object.keys(dict[key]).map((subKey) => {
+                    return <li>{subKey} :  {dict[key][subKey]}</li>;
+                })}
+            </ul>;
+        }
+        return <li>{key} :  {dict[key]}</li>;
+    })};
+
+    //@ts-ignore
+    return <div class='tab-content' hidden={!active}>
+        <ul>
+            {renderFields()}
+        </ul>
+    </div>;
+};
+
+const DataTab: FunctionComponent<{ data: any, active: boolean}> = ({ data, active }) => {
+    //@ts-ignore
+    return <div class='tab-content' hidden={!active}>
+        {data}
     </div>;
 }
--- src/out/index.d.ts
+++ src/out/index.d.ts
@@ -1,1 +1,1 @@
-export {};
+import './style.css';
